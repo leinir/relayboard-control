e8600ee2e5da3dc7075afc6848ab6dd7936480c6
Dan Leinir Turthra Jensen <admin@leinir.dk>
2021-12-22 15:43:01 +0000

    Adapt MqttClient to InputHandler refactoring, and add publishing for InputChannels

--- mqttclient.cpp
+++ mqttclient.cpp
@@ -5,21 +5,21 @@
 
 class Subscription {
 public:
-    Subscription(MqttClient *q, Config *config, QMqttSubscription *subscription)
+    Subscription(MqttClient *q, InputHandler::RelayChannel channel, QMqttSubscription *subscription)
         : q(q)
-        , config(config)
+        , channel(channel)
         , subscription(subscription)
     {
-        QObject::connect(subscription, &QMqttSubscription::messageReceived, q, [this,config,subscription,q](const QMqttMessage &msg){
+        QObject::connect(subscription, &QMqttSubscription::messageReceived, q, [channel,subscription,q](const QMqttMessage &msg){
             qDebug() << "Received message" << msg.payload() << "for topic" << subscription->topic().filter();
-            q->inputHandler()->handleKeyPressed(config->charForTopic(subscription->topic().filter()));
+            q->inputHandler()->pulseRelay(channel);
         });
         QObject::connect(subscription, &QMqttSubscription::stateChanged, q, [this](){});
         QObject::connect(subscription, &QMqttSubscription::qosChanged, q, [this](){});
         qDebug() << "Subscribed to" << subscription->topic().filter() << "with the parent" << q;
     }
     MqttClient* q;
-    Config *config;
+    InputHandler::RelayChannel channel;
     QMqttSubscription *subscription;
 };
 
@@ -35,11 +35,23 @@ public:
     QMqttClient *client{nullptr};
     QList<Subscription> subscriptions;
 
-    void handleSubscription(QMqttSubscription *sub) {
+    void handleSubscription(QMqttSubscription *sub, int channelNumber) {
         if (sub) {
-            subscriptions << Subscription(q, config, sub);
+            subscriptions << Subscription(q, inputHandler->channelByNumber(channelNumber), sub);
         } else {
             qWarning() << "Could not subscribe! Is the connection valid?";
+        }
+    }
+    void handleInputChannelStateChange(InputHandler::InputChannel channel, const QString& updatedState) {
+        static const QMetaEnum theEnum = QMetaEnum::fromType<InputHandler::InputChannel>();
+        if (channel != InputHandler::InputChannelInvalid) {
+            QString theKey{theEnum.valueToKey(channel)};
+            theKey.remove("InputChannel");
+            int channelNumber = theKey.toInt();
+            if (channelNumber > -1 && channelNumber < config->statusTopics().count()) {
+                const QString topicToUpdate{config->statusTopics().value(channelNumber - 1)};
+                client->publish(topicToUpdate, updatedState.toLatin1(), 0, true);
+            }
         }
     }
 };
@@ -76,9 +88,15 @@ void MqttClient::start()
                 qDebug() << "Connecting to MQTT broker...";
                 break;
             case QMqttClient::Connected:
+                int channelNumber{1};
                 for (const QString &topic : d->config->toggleTopics()) {
-                    d->handleSubscription(d->client->subscribe(topic, 1));
+                    d->handleSubscription(d->client->subscribe(topic, 1), channelNumber);
+                    ++channelNumber;
                 }
+                connect(d->inputHandler, &InputHandler::inputChannelStateChanged,
+                        this, [this](InputHandler::InputChannel channel, const QString& updatedState){
+                            d->handleInputChannelStateChange(channel, updatedState);
+                        });
                 break;
         }
     });
